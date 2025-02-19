89e2cf824986cbf339062a8bb6e8fa4d163f592b
Brandon Davis <neusfear@gmail.com>
2025-02-19 11:14:52 -0600

    Add hex gird map with pan zoom controls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,58 @@
-import Image from "next/image";
+"use client"
+import {TransformComponent, TransformWrapper} from "react-zoom-pan-pinch";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    //Hexagon Magic Numbers
+    const width = 90;
+    const height = 104;
+    const topOffset = 77.25;
+    const bottomMargin = 51.25;
+
+    //Map information
+    const numRows = 24;
+    const numHexes = numRows * numRows;
+    const padding = 48;
+
+    return (
+        <div className="w-screen h-screen bg-blue-400">
+            <TransformWrapper
+                centerOnInit={true}
+                wheel={{smoothStep: 0.002}}
+                minScale={0.45}
+                doubleClick={{mode: "reset"}}
+                initialScale={.45}
+            >
+                <TransformComponent
+                    wrapperClass="bg-blue-300"
+                    wrapperStyle={{width:'100%', height:'100%'}}
+                >
+                    <div className="grid"
+                         style={{
+                             gridTemplateColumns:"repeat(" + numRows * 2 + ", minmax(0, 1fr))",
+                             width: numRows * width + (padding * 2),
+                             paddingLeft: padding,
+                             paddingRight: padding,
+                             paddingTop: padding,
+                             paddingBottom: padding,
+                         }}>
+                        {Array.from({ length: numHexes }).map((_, i) => (
+                            <Hex key={i} number={i + 1} width={width} height={height} topOffset={topOffset} bottomMargin={bottomMargin} />
+                        ))}
+                    </div>
+                </TransformComponent>
+            </TransformWrapper>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
+}
+
+function Hex({ number, width, height, topOffset, bottomMargin }: {number: number, width: number, height: number, topOffset: number, bottomMargin: number}) {
+  return (
+      <div
+          className={"hexagon bg-white relative transform hover:bg-amber-400 flex flex-row content-center justify-center items-center text-black text-[5px]"}
+          style={{width: width, height: height, top: number % 2 == 0 ? 0 : topOffset, marginBottom: bottomMargin}}
+      >
+          {number}
+      </div>
+  )
 }
