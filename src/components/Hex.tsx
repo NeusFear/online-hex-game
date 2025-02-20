e4f36723b76c09a3ae031eabe2e7d266a7776e78
Brandon Davis <neusfear@gmail.com>
2025-02-19 20:50:27 -0600

    Add visual representations of resources

--- src/components/Hex.tsx
+++ src/components/Hex.tsx
@@ -1,5 +1,4 @@
 import {HexInfo} from "@/util/hexInfo";
-import {ResourceTypes} from "@/configs/biomeConfigs";
 
 export default function Hex({ hexInfo, width, height, topOffset, bottomMargin, handleSelection }:
                             {
@@ -12,21 +11,41 @@ export default function Hex({ hexInfo, width, height, topOffset, bottomMargin, h
                             }) {
 
     return (
-        <div
-            className={hexInfo.biome.color + " hexagon relative transform hover:opacity-75 flex flex-col content-center justify-center items-center text-black text-[5px]"}
-            style={{
-                width: width,
-                height: height,
-                top: hexInfo.number % 2 == 0 ? 0 : topOffset,
-                marginBottom: bottomMargin,
-                background: "url(" + hexInfo.biome.background +")",
-                backgroundPosition: "50% 50%",
-                backgroundSize: "102%",
-            }}
-            onClick={() => {handleSelection(hexInfo)}}
-        >
+        <div className={hexInfo.biome.color + " hexagon relative transform hover:opacity-75"}
+             style={{
+                 width: width,
+                 height: height,
+                 top: hexInfo.number % 2 == 0 ? 0 : topOffset,
+                 marginBottom: bottomMargin,
+                 background: "url(" + hexInfo.biome.background +")",
+                 backgroundPosition: "50% 50%",
+                 backgroundSize: "102%",
+             }}
+             onClick={() => {handleSelection(hexInfo)}}>
+            <HexResource hexInfo={hexInfo} />
+        </div>
+    )
+}
+
+function HexResource({hexInfo}: {hexInfo: HexInfo}) {
+    return(
+        <div className={"relative top-0 left-0 w-full h-full bg-red-900 items-center justify-center flex"}
+             style={{
+                 background: "url(resource_tiles/" + hexInfo.resource.type.name + "_" + hexInfo.resource.quality +".png)",
+                 backgroundPosition: "50% 50%",
+                 backgroundSize: "80%",
+                 backgroundRepeat: "no-repeat",
+             }}>
+            <HexLabel hexInfo={hexInfo} />
+        </div>
+    )
+}
+
+function HexLabel({hexInfo}: {hexInfo: HexInfo}) {
+    return (
+        <div className={"relative top-0 left-0 w-full h-full flex flex-col content-center justify-center items-center text-black text-[5px] z-50"}>
             <p>{hexInfo.biome.name}</p>
-            {hexInfo.resource.type != ResourceTypes.NONE && <p className="text-yellow-300 font-bold bg-black px-2 rounded-sm">{hexInfo.resource.type.name}: {hexInfo.resource.quality}</p>}
+            <p>{hexInfo.resource.type.name} ({hexInfo.resource.quality})</p>
         </div>
     )
 }
--- src/configs/biomeConfigs.ts
+++ src/configs/biomeConfigs.ts
@@ -24,6 +24,7 @@ export class ResourceType {
 export const ResourceTypes = {
     NONE: new ResourceType('none'),
     LUMBER: new ResourceType("lumber"),
+    CACTI: new ResourceType("cacti"),
     STONE: new ResourceType("stone"),
     IRON: new ResourceType("iron"),
     TRASH: new ResourceType("trash"),
@@ -59,13 +60,13 @@ export const Biomes = {
     PLAINS: new Biome("plains", "bg-green-300", "terrain_tiles/grass.png", new Map<ResourceType, number>([
         [ResourceTypes.STONE, 0.1],
         [ResourceTypes.IRON, 0.1],
-        [ResourceTypes.TRASH, 0.1],
+        [ResourceTypes.TRASH, 0.025],
     ])),
-    FORREST: new Biome("forest", "bg-green-600", "terrain_tiles/forrest.png", new Map<ResourceType, number>([
+    FORREST: new Biome("forest", "bg-green-600", "terrain_tiles/grass.png", new Map<ResourceType, number>([
         [ResourceTypes.LUMBER, 1],
     ])),
     OCEAN: new Biome("ocean", "bg-blue-300", "", new Map<ResourceType, number>([
-        [ResourceTypes.TRASH, 0.1],
+        [ResourceTypes.TRASH, 0.025],
     ])),
     MOUNTAIN: new Biome("mountain", "bg-gray-300", "terrain_tiles/stone.png", new Map<ResourceType, number>([
         [ResourceTypes.STONE, 1],
@@ -74,6 +75,7 @@ export const Biomes = {
     DESERT: new Biome("desert", "bg-yellow-500", "terrain_tiles/sand.png", new Map<ResourceType, number>([
         [ResourceTypes.STONE, 0.1],
         [ResourceTypes.IRON, 0.2],
+        [ResourceTypes.CACTI, 0.1]
     ])),
     MESA: new Biome("mesa", "bg-yellow-700", "terrain_tiles/mesa.png", new Map<ResourceType, number>([
         [ResourceTypes.STONE, 0.1],
